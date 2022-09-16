5adb026bddf5b26ba348de3d77c7c3abdd88d3ec
Ahsan Aman <76155759+ahsankhanan123@users.noreply.github.com>
2022-09-15 23:16:59 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+var toggle = document.querySelector(".toggle");
+var nav = document.querySelector(".nav");
+
+toggle.addEventListener("click", function () {
+ toggle.classList.toggle("active");
+ nav.classList.toggle("active");
+});
+
+
+
